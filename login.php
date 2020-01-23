9971574a38dd5033f3953e78fa40d1eb570c4e56
Rodrigo Mozo <rodrigo.mozo.01@gmail.com>
2020-01-22 23:08:00 -0500

    Login listo

--- login.php
+++ login.php
@@ -1,34 +1,48 @@
+<?php 
+session_start();
+#te regresa a index si es que ya iniciaste sesion con tu usuario
+if(isset($_SESSION["id"])){
+    header("Location: ../index.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Supervision de empleados</title>
+    <link rel="stylesheet" href="hojaestilos.css">
+    <title>Sesion</title>
 </head>
 <body>
-    <h1>Empleados tardones</h1>
-    <div>
-        <form action="index.php" method="post">
+    <div class="contenido">
+    <?php if(isset($_GET["m"])){ 
+        if($_GET["m"]=="p"){ ?>
+    <p style="color:red">El password es incorrecto</p>
+    <?php } if($_GET["m"]=="u"){ ?>
+    <p style="color:red">El nombre de usuario es incorrecto</p>
+    <?php }} ?>
+        <form action="procesar-login.php" method="post">
         
-            <div >
+            <div class="Formulario">
                 <h2>Ingresar con tu cuenta</h2>
-                    <div >
-                        Nombre de usuario:<br>
-                        <input type="text" name="u" id="" placeholder="nombre de usuario" autofocus required>
-                        </div>
-                        <div >
-                        Contraseña:<br>
-                        <input type="password" name="p" id="" placeholder="contraseña" required>
-                        </div>
-                        <div class="elemento iniciada">
-                        <input type="checkbox" name="s" value="1">
-                        Mantener sesión iniciada
-                        </div>
-                        <button type="submit" class="elemento">Entrar</button>
-                        
+                    <div class="elemento">
+                    Nombre de usuario:<br>
+                    <input type="text" name="u" id="" placeholder="nombre de usuario" autofocus required>
+                    </div>
+                    <div class="elemento">
+                    Contraseña:<br>
+                    <input type="password" name="p" id="" placeholder="contraseña" required>
+                    </div>
+                    <div class="elemento iniciada">
+                    <input type="checkbox" name="s" value="1">
+                    Mantener sesión iniciada
+                    </div>
+                    <button type="submit" class="elemento">Entrar</button>
+                    
             </div>
-            
+                
 
         </form>
     </div>
---
+++ procesar-login.php
@@ -0,0 +1,60 @@
+<?php
+
+#verifica se se entro por post o si se entro con boton regresar
+if($_SERVER['REQUEST_METHOD']!='POST' || isset($_POST["b"])){
+    header("Location: index.php");
+    exit();
+}
+
+#crear pdo
+$pdo=new PDO("mysql:host=localhost;dbname=proyectot;charset=utf8","root","");
+
+#leer datos de post
+$usuario=$_POST["u"];
+$password=$_POST["p"];
+$regresar=$_POST["b"];
+
+#construir comando
+$sql="SELECT * FROM usuarios
+WHERE Nombres = '$usuario'";
+
+#ejecutar comando
+$resultado=$pdo->query($sql);
+$fila=$resultado->fetchAll();
+
+#verifica si hay un usuario con el nombre de usuario proporcionado
+if(count($fila)!=0){
+    if($fila[0]["Contraseña"]==$password){
+        #login satisfactorio
+        #se inicia la sesion y se guardan las variables id, usuario, nombres, apellidos y email
+        session_start();
+        $_SESSION["id"]=$fila[0]["Id"];
+        
+        $_SESSION["nombres"]=$fila[0]["Nombre"];
+        $_SESSION["apellidos"]=$fila[0]["Apellidos"];
+        $_SESSION["correo"]=$fila[0]["Correo"];
+        $_SESSION["contraseña"]=$fila[0]["Contraseña"];
+
+        #entra si desea crear la cookie
+        if(isset($_POST["s"])){
+            #verificar que no exista la cookie
+            if(!isset($_COOKIE["id_usuario"])){
+                #crear cookie id_usuario con valor del id de usuario proporcionado, duracion un mes
+                setcookie("id_usuario",$fila[0]["Id"], time()+(60*60*24*30));
+            }
+        }
+        #regresar a index
+        header("Location: index.php");
+    }else{
+        #si el pass es diferente regresar a login con m=p
+        header("Location: login.php?m=p");
+        exit();
+    }
+    
+}else{
+    #si no hay usuario regresar a login con variable m=u
+    header("Location: login.php?m=u");
+    exit();
+}
+
+?>
