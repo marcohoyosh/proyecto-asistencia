81e9d0c73bc0a0f5729e4d7f09c1d95ec0094bf4
Rodrigo Mozo <rodrigo.mozo.01@gmail.com>
2020-01-23 14:11:58 -0500

    login final, ahora si

--- login.php
+++ login.php
@@ -1,11 +1,45 @@
-<?php 
+<?php
+
+#inicia sesion
 session_start();
-#te regresa a index si es que ya iniciaste sesion con tu usuario
-if(isset($_SESSION["id"])){
-    header("Location: ../index.php");
-    exit();
+
+#entra si no existe la variable id guardada en la sesion
+if(!isset($_SESSION["id"])){
+    
+    #entra si existe una cookie guardada con la id del usuario
+    if(isset($_COOKIE["id_usuario"])){
+        $id=$_COOKIE["id_usuario"];
+        
+        #crear pdo
+        $pdo=new PDO("mysql:host=localhost;dbname=$dbname;charset=utf8","root","");
+    
+        #construir comando
+        $sql="SELECT * 
+        FROM $tab_usuario 
+        WHERE $id_usuario = '$id'";
+    
+        #ejecutar comando
+        $resultado=$pdo->query($sql);
+        $fila=$resultado->fetch();
+    
+        #se guardan las variables usuario, nombres, apellidos y email
+        $_SESSION["id"]=$id;
+        $_SESSION["usuario"]=$fila[$user_usuario];
+        $_SESSION["nombres"]=$fila[$nom_usuario];
+        $_SESSION["apellidos"]=$fila[$ape_usuario];
+        $_SESSION["email"]=$fila[$email_usuario];
+        $_SESSION["direccion"]=$fila[$dir_usuario];
+        $_SESSION["fecha_nac"]=$fila[$fecha_nac_usuario];
+    }
+} else{
+    if(isset($_SESSION["id"])){
+        header("Location: ../index.php");
+        exit();
+
 }
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
