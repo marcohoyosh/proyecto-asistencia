c7c15b5b2e078da1c072d7b7fbd1eafb64d9a149
Marco Hoyos <marcohoyosh@gmail.com>
2020-01-24 09:37:48 -0500

    database

--- bd/asistencia.sql
+++ bd/asistencia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-01-2020 a las 17:28:03
+-- Tiempo de generación: 24-01-2020 a las 15:36:31
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.2.26
 
@@ -31,11 +31,31 @@ SET time_zone = "+00:00";
 CREATE TABLE `checkin` (
   `idempleado` int(11) NOT NULL,
   `fecha` date NOT NULL,
-  `horarioentrada` int(11) NOT NULL,
-  `horariosalida` int(11) NOT NULL,
-  `checkin` int(11) NOT NULL,
-  `checkout` int(11) NOT NULL
+  `horarioentrada` time NOT NULL,
+  `inciob` time NOT NULL,
+  `salidab` time NOT NULL,
+  `horariosalida` time NOT NULL,
+  `checkinicio` time NOT NULL,
+  `chekinciob` time DEFAULT NULL,
+  `checksalidab` time DEFAULT NULL,
+  `checksalida` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `checkin`
+--
+
+INSERT INTO `checkin` (`idempleado`, `fecha`, `horarioentrada`, `inciob`, `salidab`, `horariosalida`, `checkinicio`, `chekinciob`, `checksalidab`, `checksalida`) VALUES
+(25, '2020-01-07', '12:00:00', '16:00:00', '17:00:00', '22:00:00', '11:46:00', '00:00:00', '00:00:00', '22:15:00'),
+(25, '2020-01-08', '12:00:00', '16:00:00', '17:00:00', '22:00:00', '12:12:00', '00:00:00', '00:00:00', '22:21:00'),
+(25, '2020-01-09', '12:00:00', '16:00:00', '17:00:00', '22:00:00', '11:49:00', '00:00:00', '00:00:00', '22:47:00'),
+(25, '2020-01-10', '12:00:00', '16:00:00', '17:00:00', '22:00:00', '11:55:00', '00:00:00', '00:00:00', '19:53:00'),
+(25, '2020-01-11', '12:00:00', '16:00:00', '17:00:00', '22:00:00', '12:00:00', '00:00:00', '00:00:00', '21:33:00'),
+(25, '2020-01-12', '12:00:00', '16:00:00', '17:00:00', '22:00:00', '12:15:00', '00:00:00', '00:00:00', '22:15:00'),
+(25, '2020-01-13', '12:00:00', '16:00:00', '17:00:00', '22:00:00', '11:30:00', '00:00:00', '00:00:00', '22:18:00'),
+(25, '2020-01-14', '12:00:00', '16:00:00', '17:00:00', '22:00:00', '12:23:00', '00:00:00', '00:00:00', '22:24:00'),
+(25, '2020-01-15', '12:00:00', '16:00:00', '17:00:00', '22:00:00', '12:00:00', '00:00:00', '00:00:00', '23:10:00'),
+(25, '2020-01-16', '12:00:00', '16:00:00', '17:00:00', '22:00:00', '11:49:00', '00:00:00', '00:00:00', '23:58:00');
 
 -- --------------------------------------------------------
 
@@ -49,6 +69,13 @@ CREATE TABLE `departamaneto` (
   `descripcion` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `departamaneto`
+--
+
+INSERT INTO `departamaneto` (`iddepartamento`, `nombre`, `descripcion`) VALUES
+(1, 'ventas', 'area de ventas');
+
 -- --------------------------------------------------------
 
 --
@@ -58,9 +85,16 @@ CREATE TABLE `departamaneto` (
 CREATE TABLE `empleados` (
   `idempleado` int(11) NOT NULL,
   `nombre` varchar(30) NOT NULL,
-  `apellido` int(30) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
   `iddepartamento` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `empleados`
+--
+
+INSERT INTO `empleados` (`idempleado`, `nombre`, `apellido`, `iddepartamento`) VALUES
+(25, 'jorge', 'perez', 1);
 
 -- --------------------------------------------------------
 
@@ -82,7 +116,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`Id`, `Nombres`, `Apellidos`, `Correo`, `Contraseña`, `Rol`) VALUES
-(1, 'Marco', 'Hoyos', 'maro.hoyos@gmail.com', '123456', 0);
+(1, 'Marco', 'Hoyos', 'maro.hoyos@gmail.com', '123456', 0),
+(2, 'rodrigo', 'mozo', 'rodrigomozo@gmail.com', '654321', 0);
 
 --
 -- Índices para tablas volcadas
