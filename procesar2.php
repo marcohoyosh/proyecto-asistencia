5196bad681d185c3118f0d1d2213d734845ec9b1
Marco Hoyos <marcohoyosh@gmail.com>
2020-01-30 17:50:09 -0500

    Inicio y fin de braek culminados

--- procesar2.php
+++ procesar2.php
@@ -9,37 +9,58 @@ function getDatos(){
     $fecha1 = $_POST["fecha1"];
     $fecha2 = $_POST["fecha2"];
   $mysqli = getConn();
-  $query = "SELECT * FROM marcaciones inner join nieto on nieto.idnieto=marcaciones.id inner join nuevo on marcaciones.id = nuevo.idempleado where nuevo.idempleado = '$id' and nuevo.fecha between '$fecha1' and '$fecha2' group by nuevo.fecha" ;
-  
+  $pdo=new PDO("mysql:host=localhost;dbname=asistencia2;charset=utf8","root","");
+  $query = "SELECT * FROM nuevo inner join nieto on nieto.idnieto=nuevo.idempleado inner join marcaciones on marcaciones.mfecha = nuevo.fecha where nuevo.idempleado = '$id' and marcaciones.id = '$id' and nuevo.fecha between '$fecha1' and '$fecha2' group by fecha";
+  $sql = "SELECT * FROM nuevo inner join nieto on nieto.idnieto=nuevo.idempleado inner join marcaciones on marcaciones.mfecha = nuevo.fecha where nuevo.idempleado = '$id' and marcaciones.id = '$id' and nuevo.fecha between '$fecha1' and '$fecha2'";
   $result = $mysqli->query($query);
-    $x = $fila['id'];
-    $y = $fila['fecha'];
-    $query2 = "SELECT * FROM marcaciones inner join nuevo on nuevo.idempleado=marcaciones.id where nuevo.idempleado = '$x' and nuevo.fecha = '$y' ";
-    $result2 = $mysqli->query($query2);
-    while($fila2=mysqli_fetch_assoc($result2)) {
-      $fechita1 = substr($fila2["marcacion"], 11,2);
-      $numfecha1 = intval($fechita1);
-      $fechita2 = substr($fila2["horibi"], 0,2);;
-      $numfecha2 = intval($fechita2);
-      
-          
-      $breaki = 0;
-      if($breaki < 0){
-        $breaki * -1;
-      }
-
-      if($numfecha1-$numfecha2 < $breaki){
-        $breaki = $numfecha1;
-      } else if($numfecha1-$numfecha2 = $breaki){
-        $breaki = $breaki;
-      }
-      if($fechita1 == $breaki){
-        break;
-      }
-    }
-  $listas ="";
-  while($fila=mysqli_fetch_assoc($result)) { 
+  //$result2 = $mysqli->query($query2);
     
+    
+  $listas ="";
+  $breaki = 0;
+  $breaki2 = 0;
+  while($fila=mysqli_fetch_assoc($result)) { 
+    $Megafecha = $fila["fecha"];
+    $MarcacionBreak = null;
+    $MarcacionBreakSalida = null;
+    foreach($pdo->query($sql) as $fila2) {
+      
+      if($Megafecha == $fila2["mfecha"]){
+        
+          $fechita1 = substr($fila2["tiempo"], 0,2);
+          $numfecha1 = intval($fechita1);
+          $fechita2 = substr($fila2["horibi"], 0,2);
+          $numfecha2 = intval($fechita2);
+          $fechita3 = substr($fila2["horibs"], 0,2);
+          $numfecha3 = intval($fechita3);
+
+          if($breaki < 0) {
+            $breaki = $breaki * (-1);
+          }
+          if($breaki2 < 0) {
+            $breaki2 = $breaki2 * (-1);
+          }
+
+          if($numfecha1-$numfecha2 < $breaki){
+            $breaki = ($numfecha1-$numfecha2);
+            $MarcacionBreak = $fila2["tiempo"];
+          } 
+          if($numfecha1-$numfecha3 <= $breaki2){
+            $breaki2 = ($numfecha1-$numfecha2);
+            $MarcacionBreakSalida = $fila2["tiempo"];
+          }
+          
+          
+        }
+        
+    }
+      
+    if($MarcacionBreak == $fila["maringreso"] || $MarcacionBreak == $fila["marsalida"]){
+      $MarcacionBreak = "No marcó inicio de Break";
+    }
+    if($MarcacionBreakSalida == $fila["maringreso"] || $MarcacionBreakSalida == $fila["marsalida"]){
+      $MarcacionBreakSalida = "No marcó fin de Break";
+    }
         $listas .= " <tr>
                                             
         <td> ".$fila['nieto']." </td>
@@ -49,13 +70,17 @@ function getDatos(){
         <td> ".$fila["horibs"]." </td>
         <td> ".$fila["horisalida"]." </td>
         <td> ".$fila["maringreso"]." </td>
-        <td> ".$fila["maribi"]." </td>
-        <td> ".$fila["maribs"]." </td>
+        <td> ".$MarcacionBreak." </td>
+        <td> ".$MarcacionBreakSalida." </td>
         <td> ".$fila["marsalida"]." </td>
         <td> ".$fila["tardanza"]." </td>
         <td> ".$fila["temprano"]." </td>
         <td> ".$fila["worktime"]." </td>
         <td> ".$fila["tiempototal"]." </td>
+        <td> ".$fila["id"]." </td>
+        <td> ".$fila["marcacion"]." </td>
+        
+        
    
     </tr>";
   }  
